d24c1d51a145283b51bb9600af9aa98a2ab0505c
andreimdv <moldovanndrei@gmail.com>
2019-06-20 22:16:05 +0300

    csv for bat btc eth

--- data_loading/data_loader_factory.py
+++ data_loading/data_loader_factory.py
@@ -14,6 +14,7 @@ def get_data_loader(csv_source, days_to_predict, percentage_normalizer, sequence
     if 'datahub' in csv_source:
         if multiple_features:
             columns = ['price(USD)', 'exchangeVolume(USD)', 'fees', 'averageDifficulty', 'activeAddresses']
+            print('using multiple features!')
         else:
             columns = ['price(USD)']
 
